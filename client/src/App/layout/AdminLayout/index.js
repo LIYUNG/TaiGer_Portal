a5ae4fa731af3ee2a690fbd8e04d6ec8dcb7237e
Leo <liyung.chen@tum.de>
2023-01-22 17:39:53 +0100

    add footer

--- client/src/App/layout/AdminLayout/index.js
+++ client/src/App/layout/AdminLayout/index.js
@@ -214,6 +214,26 @@ function AdminLayout(props) {
               </div>
             </div>
           </div>
+          <footer
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              padding: '2px',
+              backgroundColor: '#000000'
+            }}
+          >
+            <p
+              className="my-1 text-light"
+              style={{
+                fontSize: '12px'
+              }}
+            >
+              Copyright TaiGer Consultancy © 2023 | Designed and developed by{' '}
+              <a href="https://taigerconsultancy.com/" className="text-info">
+                TaiGer Consultancy
+              </a>
+            </p>
+          </footer>
         </Fullscreen>
       </Aux>
     );

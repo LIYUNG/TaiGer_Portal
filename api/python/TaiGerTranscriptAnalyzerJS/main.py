fe47f9d54fca52849b58c5bbdf0de23d40a3674c
Leo <liyung.chen@tum.de>
2023-03-08 23:35:35 +0100

    add python version print to debug

--- api/python/TaiGerTranscriptAnalyzerJS/main.py
+++ api/python/TaiGerTranscriptAnalyzerJS/main.py
@@ -17,7 +17,11 @@ file_path = os.path.realpath(__file__)
 file_path = os.path.dirname(file_path)
 
 if __name__ == "__main__":
+    print("--------------------------")
     print("New Transcript Analyser")
+    print("Python version:")
+    print(sys.version)
+    print("--------------------------")
     ## print course:
     obj = json.loads(sys.argv[1])
     obj_arr = json.loads(obj)

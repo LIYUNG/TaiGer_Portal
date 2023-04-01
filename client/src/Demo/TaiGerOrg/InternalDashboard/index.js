24a51410f514aae3fcd3a27d18056d7e0c6e28a1
Leo <liyung.chen@tum.de>
2023-04-01 21:20:45 +0200

    add applications, admissions chart

--- client/src/Demo/TaiGerOrg/InternalDashboard/index.js
+++ client/src/Demo/TaiGerOrg/InternalDashboard/index.js
@@ -6,7 +6,10 @@ import NVD3Chart from 'react-nvd3';
 import Aux from '../../../hoc/_Aux';
 import { spinner_style } from '../../Utils/contants';
 import ErrorPage from '../../Utils/ErrorPage';
-import { is_TaiGer_role } from '../../Utils/checking-functions';
+import {
+  is_TaiGer_role,
+  programs_refactor
+} from '../../Utils/checking-functions';
 
 import { getStatistics } from '../../../api';
 import { TabTitle } from '../../Utils/TabTitle';
@@ -22,6 +25,7 @@ class InternalDashboard extends React.Component {
     teams: null,
     students: null,
     documents: null,
+    students_details: null,
     agents: null,
     editors: null,
     res_status: 0
@@ -30,8 +34,15 @@ class InternalDashboard extends React.Component {
   componentDidMount() {
     getStatistics().then(
       (resp) => {
-        const { data, success, students, agents, editors, documents } =
-          resp.data;
+        const {
+          data,
+          success,
+          students,
+          agents,
+          editors,
+          documents,
+          students_details
+        } = resp.data;
         const { status } = resp;
         if (success) {
           this.setState({
@@ -41,6 +52,7 @@ class InternalDashboard extends React.Component {
             documents: documents,
             agents: agents,
             editors: editors,
+            students_details,
             success: success,
             res_status: status
           });
@@ -67,8 +79,15 @@ class InternalDashboard extends React.Component {
       return <Redirect to={`${DEMO.DASHBOARD_LINK}`} />;
     }
     TabTitle('TaiGer Dashboard');
-    const { res_status, isLoaded, students, documents, agents, editors } =
-      this.state;
+    const {
+      res_status,
+      isLoaded,
+      students,
+      documents,
+      agents,
+      editors,
+      students_details
+    } = this.state;
 
     if (
       !isLoaded &&
@@ -120,6 +139,49 @@ class InternalDashboard extends React.Component {
       documents_data.push({
         key: `${ca}`,
         y: documents[ca].count,
+        color: colors[i]
+      });
+    });
+    const application_status = ['Open', 'Close'];
+    const applications_decided = programs_refactor(students_details).filter(
+      (application) =>
+        application.program_id !== '-' && application.decided === 'O'
+    );
+    const applications_submitted = applications_decided.filter(
+      (application) => application.closed === 'O'
+    );
+    const obj = {
+      Open: applications_decided.length - applications_submitted.length,
+      Close: applications_submitted.length
+    };
+    const applications_data = [];
+    application_status.forEach((status, i) => {
+      applications_data.push({
+        key: `${status}`,
+        y: obj[status],
+        color: colors[i]
+      });
+    });
+    const admissions_data = [];
+    const admission_status = ['Admission', 'Rejection', 'Pending'];
+    const applications_admission = applications_submitted.filter(
+      (application) => application.admission === 'O'
+    );
+    const applications_rejection = applications_submitted.filter(
+      (application) => application.admission === 'X'
+    );
+    const applications_pending = applications_submitted.filter(
+      (application) => application.admission === '-'
+    );
+    const obj2 = {
+      Admission: applications_admission.length,
+      Rejection: applications_rejection.length,
+      Pending: applications_pending.length
+    };
+    admission_status.forEach((status, i) => {
+      admissions_data.push({
+        key: `${status}`,
+        y: obj2[status],
         color: colors[i]
       });
     });
@@ -231,12 +293,12 @@ class InternalDashboard extends React.Component {
                 </Card.Title>
               </Card.Header>
               <Card.Body>
-                Number of Applications [TODO]:
+                Number of Applications:
                 <NVD3Chart
                   id="chart"
                   height={300}
                   type="pieChart"
-                  datum={editors_data}
+                  datum={applications_data}
                   x="key"
                   y="y"
                 />
@@ -253,12 +315,12 @@ class InternalDashboard extends React.Component {
                 </Card.Title>
               </Card.Header>
               <Card.Body>
-                Number of Admissions [TODO]:
+                Number of Admissions:
                 <NVD3Chart
                   id="chart"
                   height={300}
                   type="pieChart"
-                  datum={editors_data}
+                  datum={admissions_data}
                   x="key"
                   y="y"
                 />

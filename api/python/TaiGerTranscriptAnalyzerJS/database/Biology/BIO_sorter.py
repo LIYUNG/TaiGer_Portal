17d41fe89963d5792abcdcbcf2bc1983bc3c9fe6
Leo <liyung.chen@tum.de>
2023-09-17 20:56:53 +0200

    fix error

--- api/python/TaiGerTranscriptAnalyzerJS/database/Biology/BIO_sorter.py
+++ api/python/TaiGerTranscriptAnalyzerJS/database/Biology/BIO_sorter.py
@@ -2,7 +2,7 @@ import xlsxwriter
 from CourseSuggestionAlgorithms import *
 from util import *
 from keywords import *
-from database.Biology.Programs import program_sort_function, column_len_array
+from database.Biology.BIO_Programs import program_sort_function, column_len_array
 from cell_formatter import red_out_failed_subject, red_out_insufficient_credit
 import pandas as pd
 import sys
@@ -15,8 +15,8 @@ def BIO_sorter(program_idx, file_path, abbrev, studentId, student_name, analysis
 
     basic_classification_en = {
         '微積分': [CALCULUS_KEY_WORDS_EN, CALCULUS_ANTI_KEY_WORDS_EN],
-        '線性代數': [MATH_LIN_ALGE_KEY_WORDS_EN, MATH_LIN_ALGE_ANTI_KEY_WORDS_EN],
-        '數學': [MATH_KEY_WORDS_EN, MATH_ANTI_KEY_WORDS_EN],
+        '線性代數': [MATH_LINEAR_ALGEBRA_KEY_WORDS_EN, MATH_LINEAR_ALGEBRA_ANTI_KEY_WORDS_EN],
+        '數學': [MATH_GENERAL_KEY_WORDS_EN, MATH_GENERAL_ANTI_KEY_WORDS_EN],
         '經濟': [ECONOMICS_KEY_WORDS_EN, ECONOMICS_ANTI_KEY_WORDS_EN],
         '企業': [BUSINESS_KEY_WORDS_EN, BUSINESS_ANTI_KEY_WORDS_EN],
         '管理': [MANAGEMENT_KEY_WORDS_EN, MANAGEMENT_ANTI_KEY_WORDS_EN],
@@ -26,7 +26,7 @@ def BIO_sorter(program_idx, file_path, abbrev, studentId, student_name, analysis
         '行銷': [MARKETING_KEY_WORDS_EN, MARKETING_ANTI_KEY_WORDS_EN],
         '作業研究': [OP_RESEARCH_KEY_WORDS_EN, OP_RESEARCH_ANTI_KEY_WORDS_EN],
         '觀察研究': [EP_RESEARCH_KEY_WORDS_EN, EP_RESEARCH_ANTI_KEY_WORDS_EN],
-        '資工': [BASIC_CS_KEY_WORDS_EN, BASIC_CS_ANTI_KEY_WORDS_EN],
+        '資工': [MGM_BASIC_CS_KEY_WORDS_EN, MGM_BASIC_CS_ANTI_KEY_WORDS_EN],
         '程式': [PROGRAMMING_LANGUAGE_KEY_WORDS_EN, PROGRAMMING_LANGUAGE_ANTI_KEY_WORDS_EN],
         '資料科學': [DATA_SCIENCE_KEY_WORDS_EN, DATA_SCIENCE_ANTI_KEY_WORDS_EN],
         '論文': [BACHELOR_THESIS_KEY_WORDS_EN, BACHELOR_THESIS_ANTI_KEY_WORDS_EN],
@@ -35,8 +35,8 @@ def BIO_sorter(program_idx, file_path, abbrev, studentId, student_name, analysis
 
     basic_classification_zh = {
         '微積分': [CALCULUS_KEY_WORDS, CALCULUS_ANTI_KEY_WORDS],
-        '線性代數': [MATH_LIN_ALGE_KEY_WORDS, MATH_LIN_ALGE_ANTI_KEY_WORDS],
-        '數學': [MATH_KEY_WORDS, MATH_ANTI_KEY_WORDS],
+        '線性代數': [MATH_LINEAR_ALGEBRA_KEY_WORDS, MATH_LINEAR_ALGEBRA_ANTI_KEY_WORDS],
+        '數學': [MATH_GENERAL_KEY_WORDS, MATH_GENERAL_ANTI_KEY_WORDS],
         '經濟': [ECONOMICS_KEY_WORDS, ECONOMICS_ANTI_KEY_WORDS],
         '企業': [BUSINESS_KEY_WORDS, BUSINESS_ANTI_KEY_WORDS],
         '管理': [MANAGEMENT_KEY_WORDS, MANAGEMENT_ANTI_KEY_WORDS],
@@ -46,7 +46,7 @@ def BIO_sorter(program_idx, file_path, abbrev, studentId, student_name, analysis
         '行銷': [MARKETING_KEY_WORDS, MARKETING_ANTI_KEY_WORDS],
         '作業研究': [OP_RESEARCH_KEY_WORDS, OP_RESEARCH_ANTI_KEY_WORDS],
         '觀察研究': [EP_RESEARCH_KEY_WORDS, EP_RESEARCH_ANTI_KEY_WORDS],
-        '資工': [BASIC_CS_KEY_WORDS, BASIC_CS_ANTI_KEY_WORDS],
+        '資工': [MGM_BASIC_CS_KEY_WORDS, MGM_BASIC_CS_ANTI_KEY_WORDS],
         '程式': [PROGRAMMING_LANGUAGE_KEY_WORDS, PROGRAMMING_LANGUAGE_ANTI_KEY_WORDS],
         '資料科學': [DATA_SCIENCE_KEY_WORDS, DATA_SCIENCE_ANTI_KEY_WORDS],
         '論文': [BACHELOR_THESIS_KEY_WORDS, BACHELOR_THESIS_ANTI_KEY_WORDS],

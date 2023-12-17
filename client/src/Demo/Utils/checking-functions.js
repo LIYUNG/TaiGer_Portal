e193119aff828cc67ba5edf64ebba39e5c999a04
Leo <liyung.chen@tum.de>
2023-12-17 22:18:24 +0100

    fix uni-assist vpd showing bug2

--- client/src/Demo/Utils/checking-functions.js
+++ client/src/Demo/Utils/checking-functions.js
@@ -1199,13 +1199,13 @@ export const is_the_uni_assist_vpd_uploaded = (application) => {
     ) {
       return true;
     }
+    if (application.uni_assist.vpd_file_path === '') {
+      return false;
+    }
     if (
       !application.uni_assist?.vpd_paid_confirmation_file_pat ||
       application.uni_assist?.vpd_paid_confirmation_file_path !== ''
     ) {
-      return false;
-    }
-    if (application.uni_assist.vpd_file_path === '') {
       return false;
     }
   }

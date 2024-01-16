9212f3411314e7724d3f44c4b7b46f162852b33b
Alexander Jensen <ajexsen@users.noreply.github.com>
2024-01-16 22:10:54 +0100

    add SurveyInput schema

---
+++ api/models/SurveyInput.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const {
+    Types: { ObjectId }
+} = require('mongoose');
+const { threadType, STUDENT_INPUT_STATUS_E } = require('./Documentthread');
+
+const contentType = ['sentence', 'paragraph', 'essay'];
+
+const surveyInputSchema = new mongoose.Schema({
+    studenId: { type: ObjectId, required: true, ref: 'User' },
+    programId: { type: ObjectId, ref: 'Program' },
+    outsourcedUserId: [{ type: ObjectId, ref: 'User' }],
+    threadType: { type: String, required: true, enum: threadType },
+    isFinalVersion: {
+        type: Boolean,
+        default: false
+    },
+    surveyContent: [{
+        questionId: String,
+        question: String,
+        answer: String,
+        // frontend styling
+        rows: String,
+        width: Number,
+        // more generic style -> for later
+        contentType: { type: String, enum: contentType }
+    }],
+    surveyStatus: {
+        type: String,
+        enum: Object.values(STUDENT_INPUT_STATUS_E),
+        default: STUDENT_INPUT_STATUS_E.EMPTY
+    },
+    createdAt: Date,
+    updatedAt: Date
+});
+
+const surveyInput = mongoose.model('surveyInput', surveyInputSchema);
+module.exports = surveyInput;

5f380134aeb924cecfddf1b3ba647893cb767726
Alexander Jensen <ajexsen@users.noreply.github.com>
2024-02-22 00:43:10 +0100

    Refactor httpLogger to not use morgan-json

--- api/services/httpLogger.js
+++ api/services/httpLogger.js
@@ -1,31 +1,22 @@
 const morgan = require('morgan');
-const json = require('morgan-json');
 const logger = require('./logger');
 
-const format = json({
-  method: ':method',
-  url: ':url',
-  status: ':status',
-  contentLength: ':res[content-length]',
-  responseTime: ':response-time'
+const httpLogger = morgan((tokens, req, res) => {
+  const method = tokens.method(req, res);
+  const url = tokens.url(req, res);
+  const status = tokens.status(req, res);
+  const contentLength = tokens.res(req, res, 'content-length');
+  const responseTime = tokens['response-time'](req, res);
+
+  logger.info('HTTP Access Log', {
+    timestamp: new Date().toString(),
+    method,
+    url,
+    status: Number(status),
+    contentLength,
+    responseTime: Number(responseTime)
+  });
 });
 
-const httpLogger = morgan(format, {
-  stream: {
-    write: (message) => {
-      const { method, url, status, contentLength, responseTime } =
-        JSON.parse(message);
-
-      logger.info('HTTP Access Log', {
-        timestamp: new Date().toString(),
-        method,
-        url,
-        status: Number(status),
-        contentLength,
-        responseTime: Number(responseTime)
-      });
-    }
-  }
-});
 
 module.exports = httpLogger;

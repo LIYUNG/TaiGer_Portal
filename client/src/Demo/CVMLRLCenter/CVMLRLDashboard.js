e51a6ed0c7623107352ec0402f8bcd8d7f7d9bc1
Leo <liyung.chen@tum.de>
2024-03-27 13:51:49 +0100

    remove redundant packages

--- client/src/Demo/CVMLRLCenter/CVMLRLDashboard.js
+++ client/src/Demo/CVMLRLCenter/CVMLRLDashboard.js
@@ -1,7 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import { Button, Tabs, Tab, Box, Typography } from '@mui/material';
 import PropTypes from 'prop-types';
-// import { matchSorter } from 'match-sorter';
 import { useTranslation } from 'react-i18next';
 
 import { c1 } from '../Utils/contants';
@@ -13,257 +12,10 @@ import ModalMain from '../Utils/ModalHandler/ModalMain';
 
 import { SetFileAsFinal } from '../../api';
 import Banner from '../../components/Banner/Banner';
-// import DEMO from '../../store/constant';
-// import { useAuth } from '../../components/AuthProvider';
 import ModalNew from '../../components/Modal';
 import { CustomTabPanel, a11yProps } from '../../components/Tabs';
 import Loading from '../../components/Loading/Loading';
 import { MuiDataGrid } from '../../components/MuiDataGrid';
-
-// function fuzzyTextFilterFn(rows, id, filterValue) {
-//   return matchSorter(rows, filterValue, { keys: [(row) => row.values[id]] });
-// }
-
-// Let the table remove the filter if the string is empty
-// fuzzyTextFilterFn.autoRemove = (val) => !val;
-
-// Define a default UI for filtering
-// function DefaultColumnFilter({
-//   column: { filterValue, preFilteredRows, setFilter }
-// }) {
-//   const count = preFilteredRows.length;
-
-//   return (
-//     <input
-//       value={filterValue || ''}
-//       onChange={(e) => {
-//         setFilter(e.target.value || undefined); // Set undefined to remove the filter entirely
-//       }}
-//       size={10}
-//       placeholder={`Search ${count} records...`}
-//     />
-//   );
-// }
-
-// function SortTable2({ columns, data, user }) {
-//   const { t } = useTranslation();
-//   const filterTypes = React.useMemo(
-//     () => ({
-//       // Add a new fuzzyTextFilterFn filter type.
-//       fuzzyText: fuzzyTextFilterFn,
-//       // Or, override the default text filter to use
-//       // "startWith"
-//       text: (rows, id, filterValue) => {
-//         return rows.filter((row) => {
-//           const rowValue = row.values[id];
-//           return rowValue !== undefined
-//             ? String(rowValue)
-//                 .toLowerCase()
-//                 .startsWith(String(filterValue).toLowerCase())
-//             : true;
-//         });
-//       }
-//     }),
-//     []
-//   );
-//   const defaultColumn = React.useMemo(
-//     () => ({
-//       // Let's set up our default Filter UI
-//       Filter: DefaultColumnFilter
-//     }),
-//     []
-//   );
-//   const {
-//     getTableProps,
-//     getTableBodyProps,
-//     headerGroups,
-//     rows,
-//     prepareRow
-//     // visibleColumns,
-//     // canPreviousPage,
-//     // canNextPage,
-//     // pageOptions,
-//     // pageCount,
-//     // gotoPage,
-//     // nextPage,
-//     // previousPage,
-//     // setPageSize,
-//     // preGlobalFilteredRows,
-//     // setGlobalFilter
-//   } = useTable(
-//     {
-//       columns,
-//       data,
-//       defaultColumn, // Be sure to pass the defaultColumn option
-//       filterTypes
-//     },
-//     useFilters, // useFilters!
-//     useGlobalFilter, // useGlobalFilter!
-//     useSortBy
-//   );
-
-//   // We don't want to render all 2000 rows for this example, so cap
-//   // it at 20 for this use case
-//   const firstPageRows = rows.slice(0, 2000);
-
-//   return (
-//     <>
-//       <Table size="small" {...getTableProps()}>
-//         <TableHead>
-//           {headerGroups.map((headerGroup, x) => (
-//             <TableRow {...headerGroup.getHeaderGroupProps()} key={x}>
-//               {headerGroup.headers.map((column, i) => (
-//                 // Add the sorting props to control sorting. For this example
-//                 // we can add them into the header props
-
-//                 <TableCell
-//                   {...column.getHeaderProps(column.getSortByToggleProps())}
-//                   key={i}
-//                 >
-//                   {column.render('Header')}
-//                   {/* <div>{column.canFilter ? column.render('Filter') : null}</div> */}
-//                   {/* Add a sort direction indicator */}
-//                   <span>
-//                     {column.isSorted
-//                       ? column.isSortedDesc
-//                         ? ' 🔽'
-//                         : ' 🔼'
-//                       : ' ⮃'}
-//                   </span>
-//                 </TableCell>
-//               ))}
-//             </TableRow>
-//           ))}
-//           {headerGroups.map((headerGroup, j) => (
-//             <TableRow {...headerGroup.getHeaderGroupProps()} key={j}>
-//               {headerGroup.headers.map((column, i) =>
-//                 // Add the sorting props to control sorting. For this example
-//                 // we can add them into the header props
-//                 [0, 2, 3, 4, 5, 9].includes(i) ? (
-//                   <TableCell key={i}>
-//                     {column.canFilter ? column.render('Filter') : null}
-//                   </TableCell>
-//                 ) : (
-//                   <TableCell key={i}></TableCell>
-//                 )
-//               )}
-//             </TableRow>
-//           ))}
-//         </TableHead>
-//         <TableBody {...getTableBodyProps()}>
-//           {firstPageRows.map((row, i) => {
-//             prepareRow(row);
-//             return (
-//               <TableRow {...row.getRowProps()} key={i}>
-//                 {row.cells.map((cell, j) => {
-//                   return j === 0 ? (
-//                     <TableCell {...cell.getCellProps()} key={j}>
-//                       <Link
-//                         target="_blank"
-//                         to={`${DEMO.STUDENT_DATABASE_STUDENTID_LINK(
-//                           row.original.student_id,
-//                           DEMO.PROFILE_HASH
-//                         )}`}
-//                         component={LinkDom}
-//                       >
-//                         <Typography fontWeight="bold">
-//                           {cell.render('Cell')}{' '}
-//                         </Typography>
-//                       </Link>
-//                     </TableCell>
-//                   ) : j === 1 ? (
-//                     <TableCell {...cell.getCellProps()} key={j}>
-//                       {cell.value && cell.value.length > 0 ? (
-//                         cell.value.map((editor, u) => (
-//                           <Link
-//                             key={u}
-//                             target="_blank"
-//                             to={`${DEMO.TEAM_EDITOR_LINK(
-//                               editor._id.toString()
-//                             )}`}
-//                             component={LinkDom}
-//                           >
-//                             <Typography>
-//                               <b>{`${editor.firstname}`}</b>
-//                             </Typography>
-//                           </Link>
-//                         ))
-//                       ) : (
-//                         <Typography fontWeight="bold">
-//                           {t('No Editor')}
-//                         </Typography>
-//                       )}
-//                     </TableCell>
-//                   ) : j === 5 ? (
-//                     <TableCell {...cell.getCellProps()} key={j}>
-//                       <Link
-//                         target="_blank"
-//                         to={DEMO.DOCUMENT_MODIFICATION_LINK(
-//                           row.original.thread_id
-//                         )}
-//                         component={LinkDom}
-//                       >
-//                         {cell.render('Cell')}
-//                       </Link>
-//                       {is_TaiGer_role(user) && (
-//                         <>
-//                           <br />
-//                           {row.original.attributes?.map(
-//                             (attribute) =>
-//                               [1, 3, 9].includes(attribute.value) && (
-//                                 <Tooltip
-//                                   key={attribute._id}
-//                                   title={
-//                                     ATTRIBUTES[attribute.value - 1].definition
-//                                   }
-//                                 >
-//                                   <Chip
-//                                     size="small"
-//                                     label={attribute.name}
-//                                     color={COLORS[attribute.value]}
-//                                   />
-//                                 </Tooltip>
-//                               )
-//                           )}
-//                         </>
-//                       )}
-//                     </TableCell>
-//                   ) : j === 6 ? (
-//                     cell.value > 14 ? (
-//                       <TableCell {...cell.getCellProps()} key={j}>
-//                         <Typography>{cell.render('Cell')}</Typography>
-//                       </TableCell>
-//                     ) : (
-//                       <TableCell {...cell.getCellProps()} key={j}>
-//                         <Typography>{cell.render('Cell')}</Typography>
-//                       </TableCell>
-//                     )
-//                   ) : j === 4 ? (
-//                     cell.value < 30 ? (
-//                       <TableCell {...cell.getCellProps()} key={j}>
-//                         <Typography>{cell.render('Cell')}</Typography>
-//                       </TableCell>
-//                     ) : (
-//                       <TableCell {...cell.getCellProps()} key={j}>
-//                         <Typography>{cell.render('Cell')}</Typography>
-//                       </TableCell>
-//                     )
-//                   ) : (
-//                     <TableCell {...cell.getCellProps()} key={j}>
-//                       {cell.render('Cell')}
-//                     </TableCell>
-//                   );
-//                 })}
-//               </TableRow>
-//             );
-//           })}
-//         </TableBody>
-//       </Table>
-//       <br />
-//       {/* <div>Showing the first 20 results of {rows.length} rows</div> */}
-//     </>
-//   );
-// }
 
 CustomTabPanel.propTypes = {
   children: PropTypes.node,
@@ -417,7 +169,7 @@ function CVMLRLDashboard(props) {
   if (!isLoaded && !cVMLRLDashboardState.students) {
     return <Loading />;
   }
-  // console.log('cVMLRLDashboardState.students in CVMLRLDashboard', cVMLRLDashboardState.students)
+
   const open_tasks_arr = open_tasks_with_editors(cVMLRLDashboardState.students);
   // const open_tasks_arr2 = open_tasks(cVMLRLDashboardState.students);
 
@@ -485,11 +237,6 @@ function CVMLRLDashboard(props) {
           notification_key={undefined}
         />
         <MuiDataGrid rows={cvmlrl_active_tasks} columns={memoizedColumns} />
-        {/* <SortTable2
-          columns={taskTashboardHeader}
-          user={user}
-          data={cvmlrl_active_tasks}
-        /> */}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={1}>
         <Banner
@@ -503,11 +250,6 @@ function CVMLRLDashboard(props) {
           notification_key={undefined}
         />
         <MuiDataGrid rows={cvmlrl_idle_tasks} columns={memoizedColumns} />
-        {/* <SortTable2
-          columns={taskTashboardHeader}
-          user={user}
-          data={cvmlrl_idle_tasks}
-        /> */}
       </CustomTabPanel>
       <CustomTabPanel value={value} index={2}>
         <Banner

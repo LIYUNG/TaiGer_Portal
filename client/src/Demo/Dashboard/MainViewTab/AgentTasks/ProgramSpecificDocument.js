ddcdf5b1a37fd5d6c74baf365ae5b5de9cfce513
Leo <liyung.chen@tum.de>
2024-04-06 23:44:02 +0200

    fix bug

--- client/src/Demo/Dashboard/MainViewTab/AgentTasks/ProgramSpecificDocument.js
+++ client/src/Demo/Dashboard/MainViewTab/AgentTasks/ProgramSpecificDocument.js
@@ -4,7 +4,8 @@ import { Link, TableCell, TableRow } from '@mui/material';
 
 import {
   application_deadline_calculator,
-  has_agent_program_specific_tasks
+  has_agent_program_specific_tasks,
+  isProgramDecided
 } from '../../../Utils/checking-functions';
 import DEMO from '../../../../store/constant';
 
@@ -13,50 +14,54 @@ function ProgramSpecificDocument(props) {
     <>
       {/* check if documents needed to be checked by Agent (Supplementary form, Curriculum Analysis) */}
       {has_agent_program_specific_tasks(props.student) &&
-        props.student.applications.map((application) =>
-          application.doc_modification_thread.map(
-            (thread) =>
-              !thread.isFinalVersion &&
-              ['Supplementary_Form', 'Curriculum_Analysis', 'Others'].includes(
-                thread.doc_thread_id?.file_type
-              ) && (
-                <TableRow key={thread._id}>
-                  <TableCell>
-                    <Link
-                      to={`${DEMO.STUDENT_DATABASE_STUDENTID_LINK(
-                        props.student._id.toString(),
-                        DEMO.APPLICATION_HASH
-                      )}`}
-                      component={LinkDom}
-                    >
-                      {props.student.firstname} {props.student.lastname}{' '}
-                    </Link>
-                  </TableCell>
-                  <TableCell>
-                    {application_deadline_calculator(
-                      props.student,
-                      application
-                    )}
-                  </TableCell>
-                  <TableCell>
-                    <Link
-                      to={DEMO.DOCUMENT_MODIFICATION_LINK(
-                        thread.doc_thread_id._id
+        props.student.applications
+          .filter((application) => isProgramDecided(application))
+          .map((application) =>
+            application.doc_modification_thread.map(
+              (thread) =>
+                !thread.isFinalVersion &&
+                [
+                  'Supplementary_Form',
+                  'Curriculum_Analysis',
+                  'Others'
+                ].includes(thread.doc_thread_id?.file_type) && (
+                  <TableRow key={thread._id}>
+                    <TableCell>
+                      <Link
+                        to={`${DEMO.STUDENT_DATABASE_STUDENTID_LINK(
+                          props.student._id.toString(),
+                          DEMO.APPLICATION_HASH
+                        )}`}
+                        component={LinkDom}
+                      >
+                        {props.student.firstname} {props.student.lastname}{' '}
+                      </Link>
+                    </TableCell>
+                    <TableCell>
+                      {application_deadline_calculator(
+                        props.student,
+                        application
                       )}
-                      component={LinkDom}
-                      target="_blank"
-                    >
-                      {application.programId.school}
-                      {'-'}
-                      {application.programId.program_name}
-                      {'-'}
-                      {thread.doc_thread_id?.file_type}
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              )
-          )
-        )}
+                    </TableCell>
+                    <TableCell>
+                      <Link
+                        to={DEMO.DOCUMENT_MODIFICATION_LINK(
+                          thread.doc_thread_id._id
+                        )}
+                        component={LinkDom}
+                        target="_blank"
+                      >
+                        {application.programId.school}
+                        {'-'}
+                        {application.programId.program_name}
+                        {'-'}
+                        {thread.doc_thread_id?.file_type}
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                )
+            )
+          )}
     </>
   );
 }

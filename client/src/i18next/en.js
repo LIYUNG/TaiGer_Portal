0dee7f84cd58a1431d68e7a29abdf47586d3632a
Alexander Jensen <ajexsen@users.noreply.github.com>
2024-04-09 22:46:46 +0200

    fix category typo in en.js

--- client/src/i18next/en.js
+++ client/src/i18next/en.js
@@ -173,7 +173,7 @@ export const translation_en = {
     'Program Conflicts': 'Program Conflicts',
     'Tasks Dashboard': 'Tasks Dashboard',
     'Program Conflict': 'Program Conflict',
-    Category: 'Calendar',
+    Category: 'Category',
     Calendar: 'Calendar',
     'Calendar Events': 'Calendar Events',
     'My Students': 'My Students',

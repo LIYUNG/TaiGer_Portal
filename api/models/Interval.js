cddb19fa602d92cbd34622afc94d7853069c54b8
YuJuChiu <ge37yuj@mytum.de>
2024-04-28 10:50:35 +0200

    added findIntervalInDocumentThreads, fixed interval collection, added testFile

--- api/models/Interval.js
+++ api/models/Interval.js
@@ -1,42 +1,44 @@
-const { ObjectId } = require('mongodb');
-const { model, Schema } = require('mongoose');
+const mongoose = require('mongoose');
+const {
+  model,
+  Schema,
+  Types: { ObjectId }
+} = require('mongoose');
 
-const IntervalSchema = new Schema({
-  thread_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Documentthread',
-    required: function(){
-        return student_id? false : true 
+const IntervalSchema = new mongoose.Schema(
+  {
+    thread_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Documentthread'
+    },
+    student_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    message_1_id: {
+      type: ObjectId,
+      required: true
+    },
+    message_2_id: {
+      type: ObjectId,
+      required: true
+    },
+    interval_type: {
+      type: String,
+      required: true
+    },
+    interval: {
+      type: Number,
+      required: true
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+      expires: 93312000 // 3 years
     }
-  },
-  student_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: function(){
-        return thread_id? false : true 
-    }
-  },
-  message_1_id: {
-    type: ObjectId,
-    required: true
-  },
-  message_2_id: {
-    type: ObjectId,
-    required: true
-  },
-  interval_type: {
-    type: String,
-    required: true
-  },
-  interval: {
-    type: {Number, default: 0},
-    required: true
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-    expires: 93312000 // 3 years
   }
-});
-
-module.exports = model('Interval', IntervalSchema);
+);
+const Interval = mongoose.model('Interval', IntervalSchema);
+module.exports = {
+  Interval
+};

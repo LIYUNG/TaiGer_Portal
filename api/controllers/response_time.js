28c986175c6be81b78ac15c1bab756c61429742d
YuJuChiu <ge37yuj@mytum.de>
2024-06-16 22:40:41 +0200

    added agents, editors in BlankLookupTable

--- api/controllers/response_time.js
+++ api/controllers/response_time.js
@@ -4,31 +4,31 @@ const { ResponseTime } = require('../models/ResponseTime');
 
 const GetResponseTimeForCommunication = async () =>
     ResponseTime.find({ student_id: { $exists: true } })
-      .populate({
-        path: 'student_id',
-        populate: [
-          { path: 'agents', model: 'User' },
-          { path: 'editors', model: 'User' }
-        ]
-      })
-      .lean()
-  
-  const GetResponseTimeForThread = async () =>
+        .populate({
+            path: 'student_id',
+            populate: [
+                { path: 'agents', model: 'User' },
+                { path: 'editors', model: 'User' }
+            ]
+        })
+        .lean()
+
+const GetResponseTimeForThread = async () =>
     ResponseTime.find({ thread_id: { $exists: true } })
-      .populate({
-        path: 'thread_id',
-        populate: {
-          path: 'student_id',
-          model: 'User',
-          populate: [
-            { path: 'agents', model: 'User' },
-            { path: 'editors', model: 'User' }
-          ]
-        }
-      })
-      .lean()
-  
-  const FileTypeMapping = {
+        .populate({
+            path: 'thread_id',
+            populate: {
+                path: 'student_id',
+                model: 'User',
+                populate: [
+                    { path: 'agents', model: 'User' },
+                    { path: 'editors', model: 'User' }
+                ]
+            }
+        })
+        .lean()
+
+const FileTypeMapping = {
     "CV": ["CV"],
     "ML": ["ML"],
     "RL": ["RL_A", "RL_B", "RL_C", "Recommendation_Letter_A", "Recommendation_Letter_B"],
@@ -36,143 +36,149 @@ const GetResponseTimeForCommunication = async () =>
     "Communication": ["communication"],
     "Agent Support": ["Supplementary_Form", "Others", "Scholarship_Form", "Curriculum_Analysis"],
     "Portfolio": ["Portfolio"]
-  }
-  
-  const BlankLookupTable = {
+}
+
+const BlankLookupTable = {
     "UserProfile": {
-      firstname: null,
-      lastname: null,
-      role: null
+        firstname: null,
+        lastname: null,
+        role: null,
+        agents: null,
+        editors: null
     },
     "CV": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     },
     "ML": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     },
     "RL": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     },
     "Essay": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     },
     "Communication": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     },
     "Agent Support": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     },
     "Portfolio": {
-      AvgResponseTime: null,
-      ResponseTimeId: []
+        AvgResponseTime: [],
+        ResponseTimeId: []
     }
-  };
-  
-  const GetFormattedFileType = (fileType) => {
+};
+
+const GetFormattedFileType = (fileType) => {
     // Find the entry where the fileType exists in the values array
     const entry = Object.entries(FileTypeMapping).find(([key, values]) => values.includes(fileType));
     // If entry is found, return the key, otherwise return null
     return entry ? entry[0] : null;
-  };
-  
-  const GernerateLookupTable = (Lookup, key, task) => {
+};
+
+const GernerateLookupTable = (Lookup, key, task) => {
     const FormattedFileType = GetFormattedFileType(task.interval_type);
     const userId = key?._id.toString();
     if (FormattedFileType) {
-      if (!(userId in Lookup)) {
-        Lookup[userId] = JSON.parse(JSON.stringify(BlankLookupTable));
-        Lookup[userId]["UserProfile"].firstname = key.firstname;
-        Lookup[userId]["UserProfile"].lastname = key.lastname;
-        Lookup[userId]["UserProfile"].role = key.role;
-      }
-      Lookup[userId][FormattedFileType].AvgResponseTime += task.intervalAvg;
-      Lookup[userId][FormattedFileType].ResponseTimeId.push(task);
+        if (!(userId in Lookup)) {
+            Lookup[userId] = JSON.parse(JSON.stringify(BlankLookupTable));
+            Lookup[userId]["UserProfile"].firstname = key.firstname;
+            Lookup[userId]["UserProfile"].lastname = key.lastname;
+            Lookup[userId]["UserProfile"].role = key.role;
+            if (key.role === Role.Student) {
+                Lookup[userId]["UserProfile"].agents = key.agents;
+                ; Lookup[userId]["UserProfile"].editors = key.editors;
+            };
+        }
+        Lookup[userId][FormattedFileType].AvgResponseTime += task.intervalAvg;
+        Lookup[userId][FormattedFileType].ResponseTimeId.push(task);
     };
-  };
-  
-  const CalculateAvgReponseTimeinLookup = async (Lookup) => {
+};
+
+const CalculateAvgReponseTimeinLookup = async (Lookup) => {
     //calculate the average response time
     for (const user in Lookup) {
-      for (const attribute in Lookup[user]) {
-        if (attribute !== "UserProfile") {
-          const entry = Lookup[user][attribute];
-          if (entry.ResponseTimeId.length > 0) {
-            const averageResponseTime = entry.AvgResponseTime / entry.ResponseTimeId.length;
-            Lookup[user][attribute].AvgResponseTime = averageResponseTime;
-          } else {
-            Lookup[user][attribute].AvgResponseTime = null;
-          }
+        for (const attribute in Lookup[user]) {
+            if (attribute !== "UserProfile") {
+                const entry = Lookup[user][attribute];
+                if (entry.ResponseTimeId.length > 0) {
+                    const averageResponseTime = entry.AvgResponseTime / entry.ResponseTimeId.length;
+                    Lookup[user][attribute].AvgResponseTime = averageResponseTime;
+                } else {
+                    Lookup[user][attribute].AvgResponseTime = null;
+                }
+            }
         }
-      }
     }
-  };
-  
-  const GenerateResponseTimeByTaigerUser = async () => {
+};
+
+const GenerateResponseTimeByTaigerUser = async () => {
     let Lookup = {};
-  
+
     const ResponseTimeForCommunication = await GetResponseTimeForCommunication({ student_id: { $exists: true } });
     ResponseTimeForCommunication.forEach((RTFC) => {
-      if (RTFC.student_id) {
-        const { agents } = RTFC.student_id;
-        agents
-          .filter(agent => agent.firstname !== "David")
-          .forEach(agent => {
-            GernerateLookupTable(Lookup, agent, RTFC);
-          });
-      };
+        if (RTFC.student_id) {
+            const { agents } = RTFC.student_id;
+            agents
+                .filter(agent => agent.firstname !== "David")
+                .forEach(agent => {
+                    GernerateLookupTable(Lookup, agent, RTFC);
+                });
+        };
     });
-  
+
     const ResponseTimeForThread = await GetResponseTimeForThread({ thread_id: { $exists: true } });
     ResponseTimeForThread.forEach((RTFT) => {
-      if (RTFT.student_id) {
-        const { agents } = RTFT.student_id;
-        agents
-          .filter(agent => agent.firstname !== "David")
-          .forEach(agent => {
-            GernerateLookupTable(Lookup, agent, RTFT);
-          });
-  
-        const { editors } = RTFT.student_id;
-        editors
-          .forEach(editor => {
-            GernerateLookupTable(Lookup, editor, RTFT);
-          });
-      };
+        if (RTFT.student_id) {
+            const { agents } = RTFT.student_id;
+            agents
+                .filter(agent => agent.firstname !== "David")
+                .forEach(agent => {
+                    GernerateLookupTable(Lookup, agent, RTFT);
+                });
+
+            const { editors } = RTFT.student_id;
+            editors
+                .forEach(editor => {
+                    GernerateLookupTable(Lookup, editor, RTFT);
+                });
+        };
     });
-  
+
     CalculateAvgReponseTimeinLookup(Lookup);
     console.log("lookup", Lookup);
     return Lookup;
-  };
-  
-  const GenerateResponseTimeByStudent = async () => {
+};
+
+const GenerateResponseTimeByStudent = async () => {
     let Lookup = {};
-  
+
     const ResponseTimeForCommunication = await GetResponseTimeForCommunication({ student_id: { $exists: true } });
     ResponseTimeForCommunication.forEach((RTFC) => {
-      const student = RTFC.student_id ?? null
-  
-      if (student) {
-        GernerateLookupTable(Lookup, student, RTFC);
-      }
+        const student = RTFC.student_id ?? null
+
+        if (student) {
+            GernerateLookupTable(Lookup, student, RTFC);
+        }
     });
-  
+
     const ResponseTimeForThread = await GetResponseTimeForThread({ thread_id: { $exists: true } });
     ResponseTimeForThread.forEach((RTFT) => {
-      const student = RTFT.thread_id?.student_id ?? null
-  
-      if (student) {
-        GernerateLookupTable(Lookup, student, RTFT);
-      }
+        const student = RTFT.thread_id?.student_id ?? null
+
+        if (student) {
+            GernerateLookupTable(Lookup, student, RTFT);
+        }
     });
-  
+
     CalculateAvgReponseTimeinLookup(Lookup);
     console.log("lookup", Lookup);
     return Lookup;
-  };
+};

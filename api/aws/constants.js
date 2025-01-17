55137f397e32d90dedc00d3d0d0810ec30de83cf
Leo <liyung.chen@tum.de>
2025-01-17 10:08:36 +0100

    update api endpoint

--- api/aws/constants.js
+++ api/aws/constants.js
@@ -6,8 +6,8 @@ const roleToAssumeForCourseAnalyzerAPIG = isProd()
   : 'arn:aws:iam::669131042313:role/Beta-FE-LambdaStack-Beta--AuthorizedClientRoleBetaF-3H8r1NC3e8lf'; // Replace with your role ARN
 
 const apiGatewayUrl = isProd()
-  ? 'https://prod.taigerconsultancy-portal.com/analyze'
-  : 'https://beta.taigerconsultancy-portal.com/analyze'; // Replace with your API Gateway URL
+  ? 'https://prod.course.taigerconsultancy-portal.com/analyze'
+  : 'https://beta.course.taigerconsultancy-portal.com/analyze'; // Replace with your API Gateway URL
 
 module.exports = {
   roleToAssumeForCourseAnalyzerAPIG,
